ae61bda127ac4adde3b0182ce768cfcf9932e570
geraphkirgieveg <118598325+geraphkirgieveg@users.noreply.github.com>
2025-04-23 21:02:09 +0500

    Обновление игры v1.1
    Было обновлено механика отскока мяча и изменение скорости

--- Ping-pong.py
+++ Ping-pong.py
@@ -1,4 +1,5 @@
 from pygame import *
+import random
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed,wight, height):
@@ -38,8 +39,14 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-speed_x = 3
-speed_y = 3
+INIT_SPEED = 4
+MAX_SPEED = 8
+ACCEL_FACTOR = 1.05
+
+speed_x = INIT_SPEED * random.choice((-1,1))
+speed_y = random.uniform(-2,2)
+
+
 
 racket1 = Player('racket.png', 30 , 200, 4, 40 , 150)
 racket2 = Player('racket.png', 520 , 200, 4, 40 , 150)
@@ -49,6 +56,13 @@ font.init()
 font = font.SysFont('Arial', 35)
 lose1 = font.render('Игрок 1 проиграл', True, (180,0,0))
 lose2 = font.render('Игрок 2 проиграл', True, (180,0,0))
+
+def bounce_off_paddle(paddale):
+    global speed_x, speed_y
+    rel_intersect_y = (ball.rect.centery - paddale.rect.centery)/(paddale.rect.height/2)
+    speed_y = rel_intersect_y * abs(speed_x)
+    speed_x = -speed_x * ACCEL_FACTOR
+
 
 while game:
     for e in event.get():
@@ -62,16 +76,23 @@ while game:
         ball.rect.x += speed_x
         ball.rect.y -= speed_y
 
-        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-            speed_x *= -1
-            speed_y *= -1
+        if sprite.collide_rect(racket1, ball):
+            bounce_off_paddle(racket1)
+
+        if sprite.collide_rect(racket2, ball):
+            bounce_off_paddle(racket2)
 
         if ball.rect.y > 450 or ball.rect.y < 0:
             speed_y *= -1
 
         if ball.rect.x < 0:
             finish = True
-            windiw.blit(lose2,(200,200))
+            window.blit(lose2,(200,200))
+
+        if ball.rect.x > 600:
+            finish = True
+            window.blit(lose2,(200,200))
+
 
         racket1.reset()
         racket2.reset()
